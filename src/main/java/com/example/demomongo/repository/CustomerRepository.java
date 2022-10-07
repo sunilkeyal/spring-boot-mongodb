89149329c0ea65a01bba7d2d69d032fc927dabaa
Sunil Keyal <sunil.keyal@optum.com>
2022-10-07 17:04:57 -0400

    Updated lombok version

--- src/main/java/com/example/demomongo/repository/CustomerRepository.java
+++ src/main/java/com/example/demomongo/repository/CustomerRepository.java
@@ -2,7 +2,9 @@ package com.example.demomongo.repository;
 
 import com.example.demomongo.model.Customer;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends MongoRepository<Customer, String> {
 
     Customer findCustomerByFirstName(String firstName);
